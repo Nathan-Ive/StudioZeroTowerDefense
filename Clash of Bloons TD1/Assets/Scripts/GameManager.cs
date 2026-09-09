d6e3c60faff79c92d6f5d7b02ab2d72496fdd7a7
Dropsleutel69 <timvdheide2008@gmail.com>
2026-09-09 13:22:24 +0200

    Coins system

---
+++ Clash of Bloons TD1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int townCurrency = 100;
+    public TextMeshProUGUI coinText;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (coinText != null)
+        {
+            coinText.text = "Coins: " + townCurrency;
+        }
+    }
+}
